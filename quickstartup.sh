bd8a7f0dd44939cac59a7c3aec69bed8096a43b4
lukecavabarrett <luca.cavallery@gmail.com>
2020-03-03 22:31:52 +0000

    fixes

--- quickstartup.sh
+++ quickstartup.sh
@@ -3,6 +3,7 @@ sudo apt-get install -y libpthread-stubs0-dev libpq-dev libpqxx-dev libboost-all
 git pull;
 cmake .;
 make;
+openssl aes-256-cbc -in config.ini.enc -out config.ini -d -pass pass:${1} -iter 1546
 apps/sample;
 
 # usage: git clone https://github.com/lukecavabarrett/hydra.git; cd hydra; git checkout dev; sh quickstartup.sh password
