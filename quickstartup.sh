815c7c6ebb26913e8b77c5aecfa4b6c380cadf71
lukecavabarrett <luca.cavallery@gmail.com>
2020-03-05 17:39:39 +0000

    structure

--- quickstartup.sh
+++ quickstartup.sh
@@ -1,9 +1,9 @@
 sudo apt-get update -y
 sudo apt-get install -y libpthread-stubs0-dev libpq-dev  libboost-all-dev
 git pull;
+openssl aes-256-cbc -in libs/db/include/db/config.h.enc -out libs/db/include/db/config.h -d -pass pass:${1} -iter 1000000
 cmake .;
 make;
-openssl aes-256-cbc -in libs/db/include/db/config.h.enc -out libs/db/include/db/config.h -d -pass pass:${1} -iter 1000000
 apps/sample;
 
 # usage: git clone https://github.com/lukecavabarrett/hydra.git; cd hydra; git checkout dev; sh quickstartup.sh password
