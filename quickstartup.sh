5819360c31d03c4bd45d4d824796144b91b6aa6d
lukecavabarrett <luca.cavallery@gmail.com>
2020-03-06 00:54:05 +0000

    Marshall initial commit

--- quickstartup.sh
+++ quickstartup.sh
@@ -5,6 +5,6 @@ git pull;
 openssl aes-256-cbc -in libs/db/include/db/config.h.enc -out libs/db/include/db/config.h -d -pass pass:${1} -iter 1000000
 cmake .;
 make;
-apps/sample;
+apps/caput;
 
 # usage: git clone https://github.com/lukecavabarrett/hydra.git; cd hydra; git checkout dev; sh quickstartup.sh password
