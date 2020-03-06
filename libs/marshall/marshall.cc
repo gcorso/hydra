d81f5f67d5623892da1487f70a081f1123001650
lukecavabarrett <luca.cavallery@gmail.com>
2020-03-06 20:12:39 +0000

    environments

--- libs/marshall/marshall.cc
+++ libs/marshall/marshall.cc
@@ -10,16 +10,12 @@
 #include <tuple>
 #include <sstream>
 #include <cassert>
+#include <unordered_map>
+#include <unordered_set>
 
 MAKE_STREAM_STRUCT(std::cerr, "marshall: ", marshall)
 
 namespace hydra::marshall {
-
-int get_env_fd() {
-  mkdir("/tmp/nullenv", 0777);
-  int fd = open("/tmp/nullenv", O_RDWR);
-  return fd;
-}
 
 namespace buffer {
 static constexpr int BUFFER_LENGTH = 1000000;
@@ -32,18 +28,16 @@ struct dynamic_buffer {
   std::string content;
   int read() {
     int read_bytes = ::read(fd, buffer::data, buffer::BUFFER_LENGTH);
-    std::cerr << "fd"<<fd<<" read: "<<read_bytes<<" bytes! "<<std::endl;
+    std::cerr << "fd" << fd << " read: " << read_bytes << " bytes! " << std::endl;
     if (read_bytes > 0) {
       content.append(buffer::data, read_bytes);
       //std::cerr << "fd"<<fd<<" received: "<<std::string_view(buffer::data,read_bytes);
     }
     return read_bytes;
   }
-  void close() {std::cerr << "fd"<<fd<<" closed."<<std::endl;}
+  void close() { std::cerr << "fd" << fd << " closed." << std::endl; }
   inline std::size_t size() { return content.size(); }
 };
-
-
 
 namespace {
 
@@ -74,21 +68,21 @@ std::string strjoin(Ts &&... ts) {
 }
 
 namespace upload {
-  void append_stream(const uint64_t execution_id,std::string_view streamname,char *data,int size) {
-    db::execute_command(strjoin("UPDATE executions SET ",streamname," = ",streamname," || $1::bytea WHERE id = ",execution_id),db::data_binder({{data,size}}));
-  }
+void append_stream(const uint64_t execution_id, std::string_view streamname, char *data, int size) {
+  db::execute_command(strjoin("UPDATE executions SET ", streamname, " = ", streamname, " || $1::bytea WHERE id = ", execution_id), db::data_binder({{data, size}}));
+}
 }
 
 struct stream_uploader : public dynamic_buffer {
   //using dynamic_buffer::dynamic_buffer;
   const uint64_t execution_id;
   const std::string_view streamname;
-  stream_uploader(int fd,const uint64_t eid,std::string_view sn) : dynamic_buffer(fd),execution_id(eid),streamname(sn) {}
+  stream_uploader(int fd, const uint64_t eid, std::string_view sn) : dynamic_buffer(fd), execution_id(eid), streamname(sn) {}
   int read() {
     int rb = dynamic_buffer::read();
-    assert(rb>0);
-    if(rb>0)upload::append_stream(execution_id,streamname,buffer::data,rb);
-    std::cerr << streamname<<"["<<std::string_view(buffer::data,rb)<<"]"<<streamname<<std::endl;
+    assert(rb > 0);
+    if (rb > 0)upload::append_stream(execution_id, streamname, buffer::data, rb);
+    std::cerr << streamname << "[" << std::string_view(buffer::data, rb) << "]" << streamname << std::endl;
     return rb;
   }
 };
@@ -173,13 +167,52 @@ struct pollvector {
   auto size_active() { return nactive; }
   int poll(int timeout) {
     int retpoll = ::poll(fdtab, nactive, timeout);
-    std::cerr << "retpoll: "<<retpoll<<std::endl;
+    std::cerr << "retpoll: " << retpoll << std::endl;
     if (retpoll > 0)nactive = phelper<0, Tp, Ts...>::poll_loop(ptrs, active, fdtab, fdtab, nactive);
-    std::cerr << "nactive: "<<nactive<<std::endl;
+    std::cerr << "nactive: " << nactive << std::endl;
     return retpoll;
   }
 
 };
+
+namespace environment {
+
+std::unordered_set<int> env_dirs;
+
+void make_env(int env_id) {
+  //special case
+  if (env_id == 0) {
+
+
+    rmdir("/tmp/__hydraenv_0__");
+    if(mkdir("/tmp/__hydraenv_0__", 0777)){
+      log::fatal << "could not create directory: /tmp/__hydraenv_0__; errno: "<<errno<<std::endl;
+      exit(1);
+    }
+    return;
+  }
+
+  //already in set
+  if (env_dirs.find(env_id) != env_dirs.end()) {
+    system(strjoin(" git -C /tmp/__hydraenv_", env_id, "__ pull -p; git -C /tmp/__hydraenv_", env_id, "__ clean -f -d;").c_str());
+    return;
+  }
+
+  if(mkdir(strjoin(" /tmp/__hydraenv_", env_id, "__").c_str(), 0777) && errno!=EEXIST){
+    log::fatal << "could not create directory: /tmp/__hydraenv_"<<env_id<<"__; errno: "<<errno<<std::endl;
+    exit(1);
+  }
+
+  std::string gitlink = db::single_result_query(strjoin("SELECT gitlink FROM environments WHERE id = ", env_id));
+  system(strjoin("git clone ", gitlink, " /tmp/__hydraenv_", env_id, "__; git -C /tmp/__hydraenv_", env_id, "__ pull -p; git -C /tmp/__hydraenv_", env_id, "__ clean -f -d;").c_str());
+  env_dirs.insert(env_id);
+}
+
+void left_env() {
+
+}
+
+}
 
 void marshall(const uint64_t execution_id) {
   db::execution execution(execution_id);
@@ -188,7 +221,7 @@ void marshall(const uint64_t execution_id) {
   //TODO: prepare environment and get fd of directory where the job shall be run
   //TODO: fork marshall/process
 
-  int dir = get_env_fd();
+  environment::make_env(execution.environment_id);
 
   static constexpr int READ = 0, WRITE = 1;
   int out_pipe[2];
@@ -204,7 +237,7 @@ void marshall(const uint64_t execution_id) {
     //child process
 
     //change dir
-    fchdir(dir);
+    chdir(strjoin("/tmp/__hydraenv_",execution.environment_id,"__").c_str());
 
     //remap used pipes
     if (dup2(out_pipe[WRITE], STDOUT_FILENO) == -1 ||
@@ -220,9 +253,9 @@ void marshall(const uint64_t execution_id) {
     ::close(err_pipe[WRITE]);
 
     //run
-    if (execlp("bash", "bash","-c", execution.command.c_str(),nullptr) == -1) {
+    if (execlp("bash", "bash", "-c", execution.command.c_str(), nullptr) == -1) {
       std::perror("subprocess: execlp() failed");
-      log::fatal<<"errno: "<<errno<<std::endl;
+      log::fatal << "errno: " << errno << std::endl;
       exit(-1);
     }
 
@@ -233,17 +266,18 @@ void marshall(const uint64_t execution_id) {
   ::close(out_pipe[WRITE]);
   ::close(err_pipe[WRITE]);
 
-  stream_uploader su_out(out_pipe[READ],execution_id,"stdout"),su_err(err_pipe[READ],execution_id,"stderr");
+  stream_uploader su_out(out_pipe[READ], execution_id, "stdout"), su_err(err_pipe[READ], execution_id, "stderr");
 
   pollvector pv(&su_out, &su_err);
-  while(pv.size_active()){
-    if(pv.poll(2000)==0)log::marshall << "timeout" << std::endl;;
+  while (pv.size_active()) {
+    if (pv.poll(2000) == 0)log::marshall << "timeout" << std::endl;;
   }
 
   int status = 0;
   waitpid(pid, &status, 0);
   int exit_code = WEXITSTATUS(status);
   log::marshall << "exit_code: " << exit_code << std::endl;
-  db::execute_command(strjoin("UPDATE executions SET exit_code = ",exit_code," WHERE id= ",execution_id));
+  db::execute_command(strjoin("UPDATE executions SET exit_code = ", exit_code, " WHERE id= ", execution_id));
+
 }
 }
